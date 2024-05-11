711e1209e34fdc9ad2af524bc5f5a8bdf04a8ff3
hayato-matsumoto <onigiri51@iCloud.com>
2024-05-12 03:51:06 +0900

    画像変更

--- src/components/portfolio/portfolio.jsx
+++ src/components/portfolio/portfolio.jsx
@@ -8,9 +8,7 @@ const Portfolio = () => {
         <div>
         <h1 className="title">PORTFOLIO</h1>
         <ul className="portfolioList">
-           <li><PlatePortfolio photo={nba1} title='バスケットバール' english='BASKET BALL' content='aaaaa'/></li>
-           <li><PlatePortfolio title='NBA観戦' english ='NBA WATCHING' /></li>
-           <li><PlatePortfolio title='読書' english='READING' /></li>
+           
         </ul>
         </div>
     )
