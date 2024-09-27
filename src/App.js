5648d80af4f834065e181445c2e6d2cb8a3e95fe
Hayato <onigiri51@iCloud.com>
2024-09-28 01:35:46 +0900

    サイトを開いたときのアニメーション追加

--- src/App.js
+++ src/App.js
@@ -1,29 +1,28 @@
-import {Home,Header,Hcomp,About,Hobby,Portfolio,Plate,Footer} from './components/index';
+import React, { useEffect, useState } from 'react';
+import { Home, Header, Hcomp, About, Hobby, Portfolio, Plate, Footer } from './components/index';
 import './App.css';
 import background from './images/background.png';
 import { RiRepeat2Fill } from 'react-icons/ri';
 
-
-
-
-
-
-
-
-
 function App() {
-  return(
-  <div  className="all">
-    <header className='header1'><Header /></header>
-    <main className='main1'>
+  const [isLoaded, setIsLoaded] = useState(false);
+
+  useEffect(() => {
+    setIsLoaded(true);
+  }, []);
+
+  return (
+    <div className={`all ${isLoaded ? 'fade-in' : ''}`}>
+      <header className='header1'><Header /></header>
+      <main className='main1'>
         <Home />
         <div id="about"><About /></div>
-        <div id= "hobby"><Hobby /></div>
+        <div id="hobby"><Hobby /></div>
         <div id="portfolio"><Portfolio /></div>
-    </main>
-    <footer className='footer1'><Footer/></footer>
-    
-  </div>
-  )}
+      </main>
+      <footer className='footer1'><Footer /></footer>
+    </div>
+  );
+}
 
 export default App;
